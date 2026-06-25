c7b25585703e329a60436e7dd74f2c622edf1167
codermanan69 <manansahni295@gmail.com>
2026-06-26 02:32:01 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const morgan = require("morgan")
-
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
+
 app.use(express.json());
-app.use(cookieParser())
-app.use(morgan("dev"))
-app.use(express.static("./public"))
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173",
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
-}))
+    origin: "https://instaclone-mrnn.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
-/* require routes */
-const authRouter = require("./routes/auth.routes")
-const postRouter = require("./routes/post.routes")
-const userRouter = require("./routes/user.routes")
+const authRouter = require("./routes/auth.routes");
+const postRouter = require("./routes/post.routes");
+const userRouter = require("./routes/user.routes");
 
-/* using routes */
-app.use("/api/auth", authRouter)
-app.use("/api/posts", postRouter)
-app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+
+app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
