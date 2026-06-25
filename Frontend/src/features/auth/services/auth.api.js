e42314e03ae04f0df08ca8eb05289b6fcfa8585d
codermanan69 <manansahni295@gmail.com>
2026-06-26 02:44:08 +0530

    dist folder

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:3000/api/auth"
+        : "/api/auth",
     withCredentials: true,
 })
 
